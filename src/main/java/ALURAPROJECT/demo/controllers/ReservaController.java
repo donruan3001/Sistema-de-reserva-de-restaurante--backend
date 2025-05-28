70f3d0a37d48e42d152b3157c7cb12ceff813461
donruan3001 <ruancardoch@gmail.com>
2025-05-28 18:34:06 -0300

    dd

--- src/main/java/ALURAPROJECT/demo/controllers/ReservaController.java
+++ src/main/java/ALURAPROJECT/demo/controllers/ReservaController.java
@@ -1,23 +1,38 @@
 package ALURAPROJECT.demo.controllers;
-
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.cglib.core.Local;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ALURAPROJECT.demo.domain.User.RepositoryUser;
-
+import ALURAPROJECT.demo.domain.User.User;
+import ALURAPROJECT.demo.domain.mesas.Mesa;
 import ALURAPROJECT.demo.domain.mesas.RepositoryChair;
+import ALURAPROJECT.demo.domain.reservas.CreateBookingDto;
+import ALURAPROJECT.demo.domain.reservas.RepositoryBooking;
+import ALURAPROJECT.demo.domain.reservas.Reserva;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 
 @RestController
-@RequestMapping("reservas")
+@RequestMapping("booking")
 public class ReservaController {
-   
 
+@Autowired
+private RepositoryBooking repositoryBooking;
 
 @Autowired
 private RepositoryChair repositoryChair;
@@ -26,4 +41,21 @@ private RepositoryChair repositoryChair;
 private RepositoryUser repositoryUser;
 
 
+@PostMapping
+@Transactional
+public ResponseEntity PostBooking(@RequestBody @Valid CreateBookingDto dados) {
+ User user = repositoryUser.findById(dados.userId())
+        .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+
+    Mesa mesa = repositoryChair.findById(dados.chairId())
+        .orElseThrow(() -> new RuntimeException("Mesa não encontrada"));
+
+    Reserva reserva = new Reserva(dados.chairId(),mesa,dados.data_reserva(),dados.status());
+    repositoryBooking.save(reserva);
+   return ResponseEntity.status(HttpStatus.CREATED).body(reserva);
+
 }
+@GetMapping()
+public String getMethodName(@RequestParam String param) {
+    return new String();
+}}
--- src/main/java/ALURAPROJECT/demo/domain/reservas/Reserva.java
+++ src/main/java/ALURAPROJECT/demo/domain/reservas/Reserva.java
@@ -14,6 +14,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,14 +22,14 @@ import lombok.Setter;
 
 @Table(name="reservas")
 @Getter
-@Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Setter
 @Entity
 public class Reserva{
    
    @Id
-       @GeneratedValue(strategy = GenerationType.IDENTITY)
+ @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
@@ -43,4 +44,13 @@ public class Reserva{
 
     @Enumerated(EnumType.STRING)
     private EnumBooking status;
+
+    public Reserva(User user,Mesa mesa, LocalDateTime data_reserva,EnumBooking status){
+        this.user=user;
+        this.mesa=mesa;
+        this.data_reserva=data_reserva;
+        this.status=status;
+    }
+
+
 }
